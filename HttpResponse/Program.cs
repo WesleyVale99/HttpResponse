3edeb4119f8e4a74b0803a81ff1508a601df4e8d
WesleyVale99 <78268026+WesleyVale99@users.noreply.github.com>
2021-03-27 04:36:10 -0300

    OK

--- HttpResponse/Program.cs
+++ HttpResponse/Program.cs
@@ -1,5 +1,7 @@
 ﻿using HttpResponse.Set;
+using Newtonsoft.Json;
 using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Net;
@@ -17,42 +19,73 @@ namespace HttpResponse
             {
                 Console.Title = "HttpWebRequest v0.1";
                 Console.WriteLine("[x] By: Wesley Vale.");
-
+                Console.WriteLine("\n");
+                Console.WriteLine("[!] one of the methods to use the program is to leave g-recaptcha-response = null \n" +
+                    "so that the program can automatically add");
+                Console.WriteLine("\n");
                 Console.WriteLine("[x] Enter a URL: ");
                 string Url = Console.ReadLine();
+
+                Console.WriteLine("[x] Enter a Token ReCaptcha: ");
+                string Token = Console.ReadLine();
 
                 Console.WriteLine("[x] Enter Method: [0 POST, 1 GET]");
                 int method = int.Parse(Console.ReadLine());
 
-                Console.WriteLine("[x] type the pause of the points ' ? ': ");
-                int index = int.Parse(Console.ReadLine());
-
-                if (Url != "" && Url.Contains("https://") && method == 0 && index > 0 || method == 1)
+                if (Url != "" && Url.Contains("https://") && Token != "" && method == 0  || method == 1)
                 {
+
                     string[] BreakPoint = Url.Split('?');
-
-                    Console.WriteLine("Request Referer: " + BreakPoint[0]);//https://
-                    Console.WriteLine("Request Method: " + (Method)method);
-
-                    HttpWebRequest request = WebRequest.CreateHttp(BreakPoint[0]);
                     if(method == 0)
                     {
-                        string breakPoint = BreakPoint[index];
+                        string addtoken =  ReCaptcha(Token);
+                        if (addtoken == "404")
+                            return;
+
+                        string Mount = "";
+                        string[] serialize = BreakPoint[1].Split('&');
+                        for(int i = 0; i < serialize.Length; i++)
+                        {
+                            string spn = serialize[i];
+                            if (spn == "g-recaptcha-response=")
+                            {
+                                spn = "g-recaptcha-response=" + addtoken;
+                            }
+                            if(i == 0)
+                            {
+                                Mount += spn;
+                            }
+                            else
+                            {
+                                Mount += "&" + spn;
+                            }
+                        }
+
+                        HttpWebRequest request = WebRequest.CreateHttp(BreakPoint[0]);
+
+                        string breakPoint = string.Concat(Mount);
                         byte[] dados = Encoding.ASCII.GetBytes(breakPoint);
-                        Console.WriteLine("Breakpoint: " + breakPoint);
-                        Console.WriteLine("Buffer: " + dados.Length);
+
+
                         
                         request.Method = ((Method)method).ToString();
-                        request.Accept = "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9";
+                        //request.Accept = "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9";
                         request.ContentLength = dados.Length;
                         request.ContentType = "application/x-www-form-urlencoded";//"multipart/form-data; boundary=----WebKitFormBoundaryZjAtXvxTO3xUgkEl";
                         request.Referer = BreakPoint[0];
                         request.UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36";
 
-                        using var stream = request.GetRequestStream();
+                        using Stream stream = request.GetRequestStream();
                         stream.Write(dados, 0, dados.Length);
                         stream.Close();
-                        
+
+
+
+                        string Formater = JsonConvert.SerializeObject(request, Formatting.Indented);
+                        Console.WriteLine("\n");
+                        Console.WriteLine("Breakpoint: " + breakPoint);
+                        Console.WriteLine(Formater);
+
                         using WebResponse response = request.GetResponse();
                         Stream streamDados = response.GetResponseStream();
                         StreamReader reader = new StreamReader(streamDados);
@@ -66,6 +99,7 @@ namespace HttpResponse
                     }
                     else if(method == 1)
                     {
+                        HttpWebRequest request = WebRequest.CreateHttp(BreakPoint[0]);
                         request.Method = ((Method)method).ToString();
                         request.ContentType = "application/x-www-form-urlencoded";
                         request.Referer = BreakPoint[0];
@@ -98,6 +132,32 @@ namespace HttpResponse
             }
 
             Process.GetCurrentProcess().WaitForExit();
+        }
+        public static string ReCaptcha(string token)
+        {
+            string Url = "https://www.google.com/recaptcha/api2/anchor?ar=2&k="+ token + "&co=aHR0cHM6Ly9wYnRvcHouc2l0ZTo0NDM.&hl=pt-BR&v=5mNs27FP3uLBP3KBPib88r1g&size=normal&cb=ltvh0f2op6yi";
+            HttpWebRequest request = WebRequest.CreateHttp(Url);
+            if(request != null)
+            {
+                request.Method = "GET";
+                request.ContentType = "application/x-www-form-urlencoded";
+                request.UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36";
+                using WebResponse response = request.GetResponse();
+                Stream streamDados = response.GetResponseStream();
+                StreamReader reader = new StreamReader(streamDados);
+                while (!reader.EndOfStream)
+                {
+                    string objResponse = reader.ReadLine();
+                    if (objResponse != null && objResponse.Contains("value="))
+                    {;
+                        string[] split = objResponse.Split('=');
+                        return split[3].Split(@"""")[1];
+                    }
+                }
+                streamDados.Close();
+                response.Close();
+            }
+            return "404";
         }
     }
 }
