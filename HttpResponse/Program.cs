6596d8c0f1918a4b8649a86a47c65359e3a2f8f7
WesleyVale99 <78268026+WesleyVale99@users.noreply.github.com>
2021-03-27 05:40:46 -0300

    OK

--- HttpResponse/Program.cs
+++ HttpResponse/Program.cs
@@ -58,14 +58,7 @@ namespace HttpResponse
                             {
                                 spn = "g-recaptcha-response=" + addtoken;
                             }
-                            if(i == 0)
-                            {
-                                Mount += spn;
-                            }
-                            else
-                            {
-                                Mount += "&" + spn;
-                            }
+                            Mount += i == 0 ? spn : "&" + spn;
                         }
 
                         HttpWebRequest request = WebRequest.CreateHttp(BreakPoint[0]);
@@ -76,11 +69,12 @@ namespace HttpResponse
 
                         
                         request.Method = ((Method)method).ToString();
-                        //request.Accept = "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9";
+                        request.Accept = "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9";
                         request.ContentLength = data.Length;
                         request.ContentType = "application/x-www-form-urlencoded";//"multipart/form-data; boundary=----WebKitFormBoundaryZjAtXvxTO3xUgkEl";
                         request.Referer = BreakPoint[0];
                         request.UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36";
+                        request.ContinueTimeout = 100;
 
                         using Stream stream = request.GetRequestStream();
                         stream.Write(data, 0, data.Length);
